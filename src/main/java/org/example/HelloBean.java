f4cd3765ebdf49f52d52e205797e54939619fdbd
Andy4433 <anderson.s.brito613@gmail.com>
2024-05-11 11:12:13 -0300

    arrumndos dependencias e começando a codar

---
+++ src/main/java/org/example/HelloBean.java
@@ -0,0 +1,44 @@
+package org.example;
+
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import java.io.Serializable;
+
+@ManagedBean
+@SessionScoped
+public class HelloBean implements Serializable {
+
+
+    private String name;
+
+
+    private int age;
+
+    private char gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+}
